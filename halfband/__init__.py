9d491346ad68494e6f4bbcdc92b951334a2208f8
Marko Kosunen <marko.kosunen@aalto.fi>
2018-01-05 14:11:32 -0800

    Test for the halfband ok

--- halfband/__init__.py
+++ halfband/__init__.py
@@ -1,5 +1,5 @@
 # halfband class 
-# Last modification by Marko Kosunen, marko.kosunen@aalto.fi, 05.01.2018 11:07
+# Last modification by Marko Kosunen, marko.kosunen@aalto.fi, 05.01.2018 14:05
 import os
 import sys
 import numpy as np
@@ -21,6 +21,8 @@ class halfband(rtl,thesdk):
     def __init__(self,*arg): 
         self.proplist = [ 'Rs' ];    #properties that can be propagated from parent
         self.Rs = 1;                 # sampling frequency
+        self.halfband_Bandwidth=0.45 # Pass band bandwidth
+        self.halfband_N=40           #Number of coeffs
         self.iptr_A = refptr();
         self.model='py';             #can be set externally, but is not propagated
         self._Z = refptr();
@@ -32,6 +34,7 @@ class halfband(rtl,thesdk):
         self.init()
 
     def init(self):
+        self.H=self.firhalfband(**{'n':self.halfband_N, 'bandwidth':self.halfband_Bandwidth})
         self.def_rtl()
         rndpart=os.path.basename(tempfile.mkstemp()[1])
         self._infile=self._rtlsimpath +'/A_' + rndpart +'.txt'
@@ -72,7 +75,14 @@ class halfband(rtl,thesdk):
             par=False
 
         if self.model=='py':
-            out=np.array(self.iptr_A.Value)
+            print(self.iptr_A.Value.shape)
+            if self.iptr_A.Value.shape[1] > self.iptr_A.Value.shape[0]:
+                out=np.convolve(np.transpose(self.iptr_A.Value)[:,0],self.H[:,0]).rehape(-1,1)
+            else:
+                print(self.H.shape)
+                out=np.convolve(self.iptr_A.Value[:,0],self.H[:,0]).reshape(-1,1)
+
+            out=out[0::2,0]
             if par:
                 queue.put(out)
             self._Z.Value=out
@@ -82,7 +92,7 @@ class halfband(rtl,thesdk):
           except:
               pass
           fid=open(self._infile,'wb')
-          np.savetxt(fid,np.transpose(self.iptr_A.Value),fmt='%.0f')
+          np.savetxt(fid,self.iptr_A.Value,fmt='%.0f')
           #np.savetxt(fid,np.transpose(inp),fmt='%.0f')
           fid.close()
           while not os.path.isfile(self._infile):
@@ -109,8 +119,81 @@ class halfband(rtl,thesdk):
           os.remove(self._infile)
           os.remove(self._outfile)
 
+    def firhalfband(self,**kwargs):
+       n=kwargs.get('n',32)
+       if np.remainder(n,2) > 0:
+           self.print_log({'type':'F', 'msg':'Number of coefficients must be even'})
+       bandwidth=kwargs.get('bandwidth',0.45) # Fs=1
+       desired=np.array([ 1, 0] )
+       bands=np.array([0, bandwidth, 0.499,0.5])
+       coeffs=sig.remez(n, bands, desired, Hz=1)
+       hb=np.zeros((2*n-1,1))
+       hb[0::2,0]=coeffs
+       hb[n-1,0]=1
+       return hb
+   
+    def export_scala(self):
+       #This is the first effort to support generators
+       bwstr=str(self.halfband_Bandwidth).replace('.','')
+       print(bwstr)
+       packagestr="halfband_BW_"+bwstr+"_N_"+str(self.halfband_N)
+       tapfile=os.path.dirname(os.path.realpath(__file__)) +"/"+packagestr+".scala"
+       fid=open(tapfile,'w')
+       msg="object "+ packagestr+" {\n"
+       fid.write(msg)
+       msg="val H=Seq("
+       fid.write(msg)
+       lines=self.H.shape[0]
+       for k in range(lines-1):
+           fid.write("%0.32f,\n" %(self.H[k]))
+       fid.write("%0.32f)\n }\n" %(self.H[lines-1]))
+       fid.close()
+
 if __name__=="__main__":
     import matplotlib.pyplot as plt
     from  halfband import *
-    t=thesdk()
-    t.print_log({'type':'I', 'msg': "This is a testing template. Enjoy"})
+    from  f2_signal_gen import *
+    from  f2_system import *
+    siggen=f2_signal_gen()
+    siggen.bbsigdict={ 'mode':'sinusoid', 'freqs':[11.0e6 , 0.45*80e6, 0.55*80e6 ], 'length':2**14, 'BBRs':160e6 };
+    siggen.Users=1
+    siggen.Txantennas=1
+    siggen.init()
+
+    h=halfband()
+    h.iptr_A.Value=siggen._Z.Value[0,:,0].reshape(-1,1)
+    h.halfband_Bandwidth=0.45
+    h.halfband_N=40
+    h.init()
+    impulse=np.r_['0', h.H, np.zeros((1024-h.H.shape[0],1))]
+    h.export_scala() 
+    h.run() 
+
+    w=np.arange(1024)/1024
+    spe1=np.fft.fft(impulse,axis=0)
+    f=plt.figure(1)
+    plt.plot(w,20*np.log10(np.abs(spe1)/np.amax(np.abs(spe1))))
+    plt.ylim((-80,3))
+    plt.grid(True)
+    f.show()
+
+    nbits=16
+    spe2=np.fft.fft(np.round(impulse*(2**(nbits-1)-1)),axis=0)
+    g=plt.figure(2)
+    plt.plot(w,20*np.log10(np.abs(spe2)/np.amax(np.abs(spe2))))
+    plt.ylim((-80,3))
+    plt.grid(True)
+    g.show()
+    
+    #spe3=np.fft.fft(h._Z.Value,axis=0)
+    fs, spe3=sig.welch(h._Z.Value,fs=80e6,nperseg=1024,return_onesided=False,scaling='spectrum',axis=0)
+    w=np.arange(spe3.shape[0])/spe3.shape[0]
+    ff=plt.figure(3)
+    plt.plot(w,10*np.log10(np.abs(spe3)/np.amax(np.abs(spe3))))
+    plt.ylim((-80,3))
+    plt.grid(True)
+    ff.show()
+
+    #Required to keep the figures open
+    input()
+
